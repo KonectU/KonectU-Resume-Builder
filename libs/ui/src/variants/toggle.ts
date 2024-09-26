6603029e084e4320e4ba358881787eac76cbae22
Konect U <kevinlewis4801@gmail.com>
2024-09-26 18:54:22 +0530

    Update toggle.ts

--- libs/ui/src/variants/toggle.ts
+++ libs/ui/src/variants/toggle.ts
@@ -1,4 +1,4 @@
-import { cva } from "class-variance-authority";
+import { cva } from "class-variance-authority"; 
 
 export const toggleVariants = cva(
   "inline-flex items-center justify-center rounded text-sm font-medium transition-colors hover:bg-secondary/60 hover:text-secondary-foreground focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-secondary data-[state=on]:text-secondary-foreground",
