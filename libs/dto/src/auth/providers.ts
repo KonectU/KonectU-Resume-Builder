fa8f1fb42fbd42c785c4f89fd16d5a3bd34acc6e
Konect U <kevinlewis4801@gmail.com>
2024-09-26 18:28:04 +0530

    Update providers.ts

--- libs/dto/src/auth/providers.ts
+++ libs/dto/src/auth/providers.ts
@@ -3,4 +3,4 @@ import { z } from "nestjs-zod/z";
 
 const authProvidersSchema = z.array(z.enum(["email", "github", "google"]));
 
-export class AuthProvidersDto extends createZodDto(authProvidersSchema) {}
+export class AuthProvidersDto extends createZodDto(authProvidersSchema) {} 
