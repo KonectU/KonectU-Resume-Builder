06f68959ce85b86ef8c786c26d292d20ba1661af
Konect U <kevinlewis4801@gmail.com>
2024-09-26 17:51:36 +0530

    Update dayjs.ts

--- apps/client/src/libs/dayjs.ts
+++ apps/client/src/libs/dayjs.ts
@@ -4,7 +4,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
 
-dayjs.extend(localizedFormat);
+dayjs.extend(localizedFormat);//day
 dayjs.extend(relativeTime);
 dayjs.extend(timezone);
 dayjs.extend(utc);
