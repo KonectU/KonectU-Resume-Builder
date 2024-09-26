d197676f4c2d072c30a16b861c3589432e4ff88f
Konect U <kevinlewis4801@gmail.com>
2024-09-26 18:51:50 +0530

    Update index.ts

--- libs/ui/src/components/index.ts
+++ libs/ui/src/components/index.ts
@@ -1,6 +1,6 @@
 export * from "./accordion";
 export * from "./alert";
-export * from "./alert-dialog";
+export * from "./alert-dialog"; 
 export * from "./aspect-ratio";
 export * from "./avatar";
 export * from "./badge";
