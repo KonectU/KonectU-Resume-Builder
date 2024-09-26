5ca3ba68feaf3ea0d94d3281f4eed93f0d3a2a73
Konect U <kevinlewis4801@gmail.com>
2024-09-26 18:27:41 +0530

    Update message.ts

--- libs/dto/src/auth/message.ts
+++ libs/dto/src/auth/message.ts
@@ -3,4 +3,4 @@ import { z } from "nestjs-zod/z";
 
 export const messageSchema = z.object({ message: z.string() });
 
-export class MessageDto extends createZodDto(messageSchema) {}
+export class MessageDto extends createZodDto(messageSchema) {} 
