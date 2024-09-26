c6729746615725d538d398d4455428f090442de3
Konect U <kevinlewis4801@gmail.com>
2024-09-26 18:59:43 +0530

    Update vite.config.ts

--- libs/utils/vite.config.ts
+++ libs/utils/vite.config.ts
@@ -2,7 +2,7 @@ import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  cacheDir: "../../node_modules/.vite/utils",
+  cacheDir: "../../node_modules/.vite/utils", 
 
   plugins: [nxViteTsPaths()],
 
