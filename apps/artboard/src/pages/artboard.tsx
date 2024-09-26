1aad521ccf302b48e0ecfa0afecdb16ee7a1fead
Konect U <kevinlewis4801@gmail.com>
2024-09-26 15:52:30 +0530

    Update artboard.tsx

--- apps/artboard/src/pages/artboard.tsx
+++ apps/artboard/src/pages/artboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo } from "react";// from react
 import { Outlet } from "react-router-dom";
 import webfontloader from "webfontloader";
 
