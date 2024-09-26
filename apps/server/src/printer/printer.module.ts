6243e3f5cd621d8df7b4d73738a3435842e498fd
Konect U <kevinlewis4801@gmail.com>
2024-09-26 18:22:53 +0530

    Update printer.module.ts

--- apps/server/src/printer/printer.module.ts
+++ apps/server/src/printer/printer.module.ts
@@ -1,4 +1,4 @@
-import { HttpModule } from "@nestjs/axios";
+import { HttpModule } from "@nestjs/axios";  
 import { Module } from "@nestjs/common";
 
 import { StorageModule } from "../storage/storage.module";
