6882176480f2321ba1bd6e15f119f3d6c3cd16c4
Konect U <kevinlewis4801@gmail.com>
2024-09-26 18:13:21 +0530

    Update upload-image.ts

--- apps/client/src/services/storage/upload-image.ts
+++ apps/client/src/services/storage/upload-image.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query"; //import
 import { AxiosResponse } from "axios";
 
 import { axios } from "@/client/libs/axios";
